d6325b2396a2ccc9a4ae00d66fc2f1658ffa11c8
itadmin <itadmin@corp.ymatou.com>
2015-12-20 16:11:42 +0800

    Using django-http-proxy

--- fdproxy/fdproxy/fdproxy/urls.py
+++ fdproxy/fdproxy/fdproxy/urls.py
@@ -4,43 +4,9 @@ Definition of urls for fdproxy.
 
 from datetime import datetime
 from django.conf.urls import patterns, url
-from app.forms import BootstrapAuthenticationForm
-from app.views import GoogleProxyView
+from httpproxy.views import HttpProxy
 
-
-
-# Uncomment the next lines to enable the admin:
-# from django.conf.urls import include
-# from django.contrib import admin
-# admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'app.views.home', name='home'),
-    # url(r'^contact$', 'app.views.contact', name='contact'),
-    # url(r'^about', 'app.views.about', name='about'),
-    #url(r'^login/$',
-    #    'django.contrib.auth.views.login',
-    #    {
-    #        'template_name': 'app/login.html',
-    #        'authentication_form': BootstrapAuthenticationForm,
-    #        'extra_context':
-    #        {
-    #            'title':'Log in',
-    #            'year':datetime.now().year,
-    #        }
-    #    },
-    #    name='login'),
-    #url(r'^logout$',
-    #    'django.contrib.auth.views.logout',
-    #    {
-    #        'next_page': '/',
-    #    },
-    #    name='logout'),
-    url(r'^(?P<path>.*)$', GoogleProxyView.as_view()),
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    (r'^proxy/(?P<url>.*)$', HttpProxy.as_view(base_url='http://www.google.com/')),
 )
